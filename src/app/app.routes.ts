bb0d33979dbda6dbe8b3ed59fcb656dc72bb1304
Pratik Satpute <164974026+pvsatput@users.noreply.github.com>
2025-02-07 19:22:36 -0500

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { OneComponent } from './one/one.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: OneComponent },
+    { path: 'about', component: OneComponent },
+    { path: 'projects', component: OneComponent },
+    { path: 'contact', component: OneComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' } // Redirect unknown paths to home
+  ];
